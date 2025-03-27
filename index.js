6000dd573a2c90b5f0578e1601c09ab25ae80647
hackcode369 <prtiiik111@gmail.com>
2025-03-27 13:26:20 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
-//Variables - A container that stores a value.
-#Variables are used to store data in a program.
+//let x =5;
+//console.log(x);
+//let price =100;
+//console.log(price);
+//let gpa =2.1;
+//console.log(gpa);
+//let firstName ="Kabir";
+//console.log(firstName);
+//let favoriteFood="Pizza"
+//console.log(favoriteFood);
+//let online = true;
+//console.log(online);
+//let forSale = false;
+//console.log(forSale);
+// Using placeholder $()
+//console.log('you are buying $' + price + ' worth of items');
+//console.log(`you are ${25} years of age`);
+//console.log(`I am ${35} years of age`);
+//console.log(`The price is $${25}`);// including $ in the string
+//console.log(`mine gpa is:${2.1}`);
+//console.log(`My name is ${firstName}`);
+//console.log(`My favorite food is ${favoriteFood}`);
+//console.log(`I am online:${online}`);
+//console.log(`Is it foreSale:${forSale}`);
+// use of typeof operator to check the data type of a variable
+//console.log(typeof x);  
+//console.log(typeof price);
+//console.log(typeof gpa);
+//console.log(typeof firstName);
+//console.log(typeof favoriteFood);
+//console.log(typeof online);
+//console.log(typeof forSale);
+
+//let fullName ="Kabir Khan";
+//let age = 35;
+//Enrolled = true;
+
+//document.getElementById("p1").textContent=`My name is ${fullName}`;
+//document.getElementById("p2").textContent=`I am ${age} years of age`;
+//document.getElementById("p3").textContent=`I am ${Enrolled}`;
+
+// Arithmetic Operators
+
+let students = 30;
+//students = students + 1; //addition 
+//students = students-1; // substaction
+//students = students * 2;// multiplication
+//students = students/3;// division
+//students = students ** 2;// exponentiation
+//students = students %4;// modulus operator
+
+//Now using augmented assignment operators
+//students += 1; //addition
+//students -= 1; // subtraction
+//students *= 2;// multiplication
+//students /= 3;// division 
+//students **= 2;// exponentiation
+//students %= 4;// modulus operator
+console.log(students);
+
